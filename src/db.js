e28c960a31c9c170934f24e30adfd56441f5a57a
leoburgos07 <leonardobh96@gmail.com>
2021-08-25 00:40:39 -0500

    Agregando conexion de DB en mongo ATLAS

--- src/db.js
+++ src/db.js
@@ -2,12 +2,12 @@ const mongoose = require('mongoose');
 
 
 
-MONGODB_URI = `mongodb://localhost/usuarios`;
+MONGODB_URI = `mongodb+srv://leoburgos7:960221leo@cluster0.vqkev.mongodb.net/usuarios?retryWrites=true&w=majority`;
 
 mongoose.connect(MONGODB_URI, {
     useUnifiedTopology : true,
     useNewUrlParser : true,
     useCreateIndex:true,
     useFindAndModify: false
-}).then(db => console.log('Base de datos conectada'))
+}).then(db => console.log('Base de datos conectada!!'))
   .catch(err => console.log(err));
